254c6cffac5e787071ef5e1ab7eb8b70939baf13
Joe Marty <jmarty@iexposure.com>
2017-01-13 11:17:01 -0600

    Add specs (#4)

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'rack/test'
+require 'webmock/rspec'
+require 'omniauth-oauth2-generic'
+
+RSpec.configure do |config|
+  config.extend OmniAuth::Test::StrategyMacros, :type => :strategy
+  config.include Rack::Test::Methods
+  config.include WebMock::API
+end
